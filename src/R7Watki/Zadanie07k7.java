459f645a4803b93ad848aaecf528c759bb8f00d0
maciek zelek <mzelechowski79@gmail.com>
2023-03-27 23:13:18 +0200

    Zadanie07k7

--- src/R7Watki/Zadanie07k7.java
+++ src/R7Watki/Zadanie07k7.java
@@ -1,9 +1,9 @@
 package R7Watki;
 
-class SumArray {
+class SumArray7 {
     private int sum;
 
-    public synchronized int sumArray(int numbers[]) {
+    int sumArray(int[] numbers) {
         sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
@@ -18,13 +18,13 @@ class SumArray {
     }
 }
 
-class MyThread6 implements Runnable {
+class MyThread7 implements Runnable {
     Thread thr;
     static SumArray sa = new SumArray();
     int a[];
     int odp;
 
-    public MyThread6(String name, int numbers[]) {
+    public MyThread7(String name, int numbers[]) {
         thr = new Thread(this, name);
         thr.start();
         a = numbers;
@@ -33,25 +33,25 @@ class MyThread6 implements Runnable {
     @Override
     public void run() {
         System.out.println(thr.getName() + " startuje.");
-        odp = sa.sumArray(a);
-
+        synchronized (sa) {
+            odp = sa.sumArray(a);
+        }
         System.out.println("Suma końcowa dla " + thr.getName() + " is " + odp + ".");
         System.out.println(thr.getName() + " został zakończony.");
     }
 }
 
-public class Zadanie07k6 {
+public class Zadanie07k7 {
     public static void main(String[] args) {
-        /*Napisz program, który ilusturej dostęp do metod poprzez, zmodyfikowanie ich
-        za pomocą słowa kluczowego synchronized. Przyjmij, że program zawiera trzy klasy.
-        Pierwsza z nich, SumArray zawiera metodę sumArray() sumujące wszystkie elementy zamieszczone w tablicy.
-        Druga klasa mójWatek, używa obietku typu sumArray do uzyskania sumy elementów tablicy skłądających się
-        z liczb całkowitych. Ostatnia klasa Main, tworzy dwa wątki, które obliczają sumę elementów zawartych
-        w tablicy.
-         */
-        int a[] ={1,2,3,4,5};
+        int a[] = {1, 2, 3, 4, 5};
 
         MyThread6 mw1 = new MyThread6("Watek 1", a);
         MyThread6 mw2 = new MyThread6("Watek 2", a);
+        try {
+            mw1.thr.join();
+            mw2.thr.join();
+        } catch (InterruptedException exc) {
+            System.out.println("The main thread has been interrupted.");
+        }
     }
 }
