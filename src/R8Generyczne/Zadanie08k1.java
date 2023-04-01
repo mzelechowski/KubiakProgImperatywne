bbd71e8eae1857aa3f3997fd33a778ab0a735e51
maciek zelek <mzelechowski79@gmail.com>
2023-04-01 15:41:10 +0200

    Zadanie08k1 oraz 08k2 generic

---
+++ src/R8Generyczne/Zadanie08k1.java
@@ -0,0 +1,44 @@
+package R8Generyczne;
+
+import java.util.ArrayList;
+
+class StosGeneryczny<E> {
+    ArrayList<E> list = new ArrayList<>();
+
+    public int rozmiarStosu() {
+        return this.list.size();
+    }
+
+    public void dodajDoStosu(E object) {
+        this.list.add(object);
+    }
+
+    public E zdejmijZeStosu() {
+        return this.list.remove(list.size() - 1);
+    }
+
+    @Override
+    public String toString() {
+        return "StosGeneryczny{" +
+                "list=" + list +
+                '}';
+    }
+}
+
+public class Zadanie08k1 {
+    public static void main(String[] args) {
+        /**Napisz program, który tworząc klasę generyczną o nazwie StosGeneryczny<E>
+         * klasa zaweira następujące metody rozmiarStosu(), dodaj do stosu(), zedjmijze stosu()
+         */
+        StosGeneryczny<String> stos = new StosGeneryczny<>();
+        stos.dodajDoStosu("Java");
+        stos.dodajDoStosu("JDBC");
+        stos.dodajDoStosu("Hibernate");
+        stos.dodajDoStosu("MAVEN");
+        System.out.println(stos);
+        System.out.println(stos.rozmiarStosu());
+        System.out.println(stos.zdejmijZeStosu());
+        System.out.println(stos.rozmiarStosu());
+        System.out.println(stos);
+    }
+}
---
+++ src/R8Generyczne/Zadanie08k2.java
@@ -0,0 +1,21 @@
+package R8Generyczne;
+
+import java.util.ArrayList;
+
+public class Zadanie08k2 {
+    public static void main(String[] args) {
+        /*Zadanie8.1 zmodyfikuja tak aby do stosu doać 4 liczby typy Integer i zdjąć dwa elemetny tego typu
+         */
+        StosGeneryczny<Integer> stos = new StosGeneryczny<>();
+        stos.dodajDoStosu(4556);
+        stos.dodajDoStosu(23423);
+        stos.dodajDoStosu(323324);
+        stos.dodajDoStosu(4434543);
+        System.out.println(stos);
+        System.out.println(stos.rozmiarStosu());
+        stos.zdejmijZeStosu();
+        stos.zdejmijZeStosu();
+        System.out.println(stos);
+        System.out.println(stos.rozmiarStosu());
+    }
+}
