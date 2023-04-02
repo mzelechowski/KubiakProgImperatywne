340353896520a394d67cead8455dde9355975305
Maciej Żelechowski <mzelechowski79@gmail.com>
2023-04-02 12:55:43 +0200

    Zadanie09k2-k3

---
+++ src/R9DatyGodziny/Zadanie09k2.java
@@ -0,0 +1,11 @@
+package R9DatyGodziny;
+
+import java.time.LocalTime;
+
+public class Zadanie09k2 {
+    public static void main(String[] args) {
+        /* Napisz program, który pozyskuje bieżacy czas z systemowego zegara komputera.
+         */
+        System.out.println("Current time is: "+ LocalTime.now()+".");
+    }
+}
---
+++ src/R9DatyGodziny/Zadanie09k3.java
@@ -0,0 +1,11 @@
+package R9DatyGodziny;
+
+import java.time.LocalDateTime;
+
+public class Zadanie09k3 {
+    public static void main(String[] args) {
+        /*Napisz program, który wyświetla aktualną datę i godzinę
+         */
+        System.out.println("Current date and time is: "+ LocalDateTime.now());
+    }
+}
